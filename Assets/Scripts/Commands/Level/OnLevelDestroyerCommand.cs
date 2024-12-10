21a071d9cebb89d9f353a696d4ab98260c11a941
Emir Han Mamak <emirhanmamak@hotmail.com>
2024-12-10 14:25:40 +0300

    Odin added, Foldering Implemented, LevelManager-PoolData-Level Scriptable Object and more added.

---
+++ Assets/Scripts/Commands/Level/OnLevelDestroyerCommand.cs
@@ -0,0 +1,20 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Commands.Level
+{
+    public class OnLevelDestroyerCommand
+    {
+        private Transform _levelHolder;
+        public OnLevelDestroyerCommand(Transform levelHolder)
+        {
+            _levelHolder = levelHolder;
+        }
+        [Button]
+        public void Execute()
+        {
+            if(_levelHolder.transform.childCount <= 0) return;
+            Object.Destroy(_levelHolder.transform.GetChild(0).gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Commands.Level;
+using Data.ValueObjects;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Managers
+{
+    public class LevelManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serilazed Variables
+
+        [SerializeField] private Transform levelHolder;
+        [SerializeField] private byte totalLevelCount;
+
+        #endregion
+
+        #region Private Variables
+
+        private OnLevelLoaderCommand _levelLoaderCommand;
+        private OnLevelDestroyerCommand _levelDestroyerCommand;
+        
+        private LevelData _levelData;
+        private byte _currentLevel;
+
+        #endregion
+
+        #endregion
+
+        private void Awake()
+        {
+            InitCommands();
+            //_levelData = GetLevelData();
+           // _currentLevel = GetActiveScene();
+        }
+
+        private void InitCommands()
+        {
+            _levelDestroyerCommand = new OnLevelDestroyerCommand(levelHolder);
+            _levelLoaderCommand = new OnLevelLoaderCommand(levelHolder);
+        }
+        [Button]
+        public void LevelLoader(byte levelIndex)
+        {
+            _levelLoaderCommand.Execute(levelIndex);
+        }
+        [Button]
+        public void LevelDestroyer()
+        {
+            _levelDestroyerCommand.Execute();
+        }
+        private LevelData GetLevelData()
+        {
+            throw new NotImplementedException();
+        }
+
+        private byte GetActiveScene()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
